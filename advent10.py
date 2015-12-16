6a58cbfabcc2358d6028f23270b03a08df0df883
Ohad Cohen <ohadcn@gmail.com>
2015-12-16 22:21:42 +0200

    add advent day 10

---
+++ advent10.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+def say(seq, times = 1):
+    if not times:
+        return seq
+    nseq=''
+    zero=ord('0')
+    rep = 1
+    bef = seq[0]
+    for i in xrange(1, len(seq)):
+        if seq[i] == bef:
+            rep+=1
+        else:
+            nseq += chr(rep+zero) + bef
+            bef = seq[i]
+            rep = 1
+    nseq += chr(rep+zero) + bef
+    return say(nseq, times - 1)
